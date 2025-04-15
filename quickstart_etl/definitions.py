420008e65e91a80b501aa0686cfe71a41a2d4812
pradeep-peddineni-amida <pradeep.peddineni@amida.com>
2025-04-15 16:23:49 -0400

    Update definitions.py

--- quickstart_etl/definitions.py
+++ quickstart_etl/definitions.py
@@ -1,8 +1,7 @@
 from dagster import Definitions
-
-from quickstart_etl.assets.copyADLSfiles import copyADLSfiles
+from quickstart_etl.assets.copyADLSfiles import process_and_count_csvs
 from quickstart_etl.assets.validate_csv_counts import validate_csv_counts
 
 defs = Definitions(
-    assets=[copyADLSfiles, validate_csv_counts],
+    assets=[process_and_count_csvs, validate_csv_counts],
 )
