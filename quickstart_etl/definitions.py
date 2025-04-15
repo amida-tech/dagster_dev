16288d21feb247f481d837aa8409197bd12865b9
pradeep-peddineni-amida <pradeep.peddineni@amida.com>
2025-04-15 16:16:03 -0400

    Update definitions.py

--- quickstart_etl/definitions.py
+++ quickstart_etl/definitions.py
@@ -2,5 +2,6 @@ from dagster import Definitions, load_assets_from_modules
 from quickstart_etl.assets import data_assets
 
 defs = Definitions(
-    assets=load_assets_from_modules([data_assets]),
+    assets=[copyADLSfiles, validate_csv_counts],
+    schedules=[daily_refresh_schedule],
 )
