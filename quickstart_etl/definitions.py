a88687426ee469d6458643943e1413518787177a
pradeep-peddineni-amida <pradeep.peddineni@amida.com>
2025-04-15 16:57:38 -0400

    Update definitions.py

--- quickstart_etl/definitions.py
+++ quickstart_etl/definitions.py
@@ -1,14 +1,17 @@
 from dagster import Definitions, define_asset_job
+
 from quickstart_etl.assets.copyADLSfiles import process_and_count_csvs
 from quickstart_etl.assets.validate_csv_counts import validate_csv_counts
+from quickstart_etl.assets.convert_csv_to_parquet import convert_csv_to_parquet
 
-# Define the asset job
-adls_dq_job = define_asset_job(
-    name="adls_dq_job",
-    selection=["process_and_count_csvs", "validate_csv_counts"],
-)
+recipient_data_load = define_asset_job(name="recipient_data_load")
 
 defs = Definitions(
-    assets=[process_and_count_csvs, validate_csv_counts],
-    jobs=[adls_dq_job]
+    assets=[
+        process_and_count_csvs,
+        validate_csv_counts,
+        convert_csv_to_parquet,
+    ],
+    jobs=[recipient_data_load],
 )
+
