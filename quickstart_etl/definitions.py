1bdaffa6af561fd2e36f525ff2a81bf22cda69ec
pradeep-peddineni-amida <pradeep.peddineni@amida.com>
2025-04-16 01:34:59 -0400

    Update definitions.py

--- quickstart_etl/definitions.py
+++ quickstart_etl/definitions.py
@@ -3,6 +3,7 @@ from dagster import Definitions, define_asset_job
 from quickstart_etl.assets.copyADLSfiles import process_and_count_csvs
 from quickstart_etl.assets.validate_csv_counts import validate_csv_counts
 from quickstart_etl.assets.convert_csv_to_parquet import convert_csv_to_parquet
+from quickstart_etl.assets.load_all_recipient_data import load_all_recipient_data
 
 recipient_data_load = define_asset_job(name="recipient_data_load")
 
@@ -11,6 +12,7 @@ defs = Definitions(
         process_and_count_csvs,
         validate_csv_counts,
         convert_csv_to_parquet,
+        load_all_recipient_data,
     ],
     jobs=[recipient_data_load],
 )
